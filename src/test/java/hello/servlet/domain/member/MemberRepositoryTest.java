882ec822e28b7a7abef2bd62dbf2b3f8764a0647
alscjf1329 <alscjf1329@gmail.com>
2023-03-08 18:18:06 +0900

    feat) Member, MemberRepository 생성 및 테스트

---
+++ src/test/java/hello/servlet/domain/member/MemberRepositoryTest.java
@@ -0,0 +1,46 @@
+package hello.servlet.domain.member;
+
+import hello.servlet.basic.member.Member;
+import hello.servlet.basic.member.MemberRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+//자동으로 Assertions를 붙여줌
+//원래는 Assertions.asserThat() 이처럼 사용해야함
+
+public class MemberRepositoryTest {
+    MemberRepository memberRepository=MemberRepository.getInstance();
+    @AfterEach
+    //Test가 끝날때마다 실행
+    void afterEach(){
+        memberRepository.clearStore();
+    }
+    @Test
+    void save(){
+        //given
+        Member member = new Member("hello",20);
+        //when
+        Member savedMember = memberRepository.save(member);
+        //then
+        Member findMember=memberRepository.findById(savedMember.getId());
+        assertThat(findMember).isEqualTo(savedMember);
+    }
+    @Test
+    void findAll(){
+        //given
+        Member member1 = new Member("member1", 20);
+        Member member2 = new Member("member2", 20);
+
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+        //when
+        List<Member> result=memberRepository.findAll();
+        //then
+        assertThat(result.size()).isEqualTo(2);
+        assertThat(result).contains(member1,member2);
+    }
+}
